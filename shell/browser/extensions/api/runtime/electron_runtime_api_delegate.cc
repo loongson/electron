c0c43dca70f9fc62bb827bb0d9941c274bf5a106
Mingtao Zhou <zhoumingtao@loongson.cn>
2023-08-10 02:54:46 +0000

    feat:add support for loongarch64

--- shell/browser/extensions/api/runtime/electron_runtime_api_delegate.cc
+++ shell/browser/extensions/api/runtime/electron_runtime_api_delegate.cc
@@ -69,6 +69,10 @@ bool ElectronRuntimeAPIDelegate::GetPlatformInfo(PlatformInfo* info) {
     info->arch = extensions::api::runtime::PlatformArch::kX86_32;
   } else if (strcmp(arch, "x64") == 0) {
     info->arch = extensions::api::runtime::PlatformArch::kX86_64;
+  } else if (strcmp(arch, "loong64") == 0) {
+    info->arch = extensions::api::runtime::PlatformArch::kLoong64;
+  } else if (strcmp(arch, "mips64el") == 0) {
+    info->arch = extensions::api::runtime::PlatformArch::kMips64el;
   } else {
     NOTREACHED();
     return false;
@@ -81,6 +85,10 @@ bool ElectronRuntimeAPIDelegate::GetPlatformInfo(PlatformInfo* info) {
     info->nacl_arch = extensions::api::runtime::PlatformNaclArch::kX86_32;
   } else if (strcmp(nacl_arch, "x86-64") == 0) {
     info->nacl_arch = extensions::api::runtime::PlatformNaclArch::kX86_64;
+  } else if (strcmp(nacl_arch, "loong64") == 0) {
+    info->nacl_arch = extensions::api::runtime::PlatformNaclArch::kLoong64;
+  } else if (strcmp(nacl_arch, "mips64el") == 0) {
+    info->nacl_arch = extensions::api::runtime::PlatformNaclArch::kMips64el;
   } else {
     NOTREACHED();
     return false;
